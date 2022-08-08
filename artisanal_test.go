2073464df61545059afb6810cc532cc1ea8b204d
thisisaaronland <devnull@localhost>
2022-08-07 22:16:15 -0700

    snapshot: compiles, no tests

--- artisanal_test.go
+++ artisanal_test.go
@@ -1,47 +1,15 @@
 package artisanal
 
 import (
-	"context"
+	_ "context"
 	_ "github.com/aaronland/go-brooklynintegers-api"
 	_ "github.com/aaronland/go-missionintegers-api"
-	"github.com/aaronland/go-uid"
+	_ "github.com/aaronland/go-uid"
 	"testing"
 )
 
 func TestArtisanalProvider(t *testing.T) {
 
-	ctx := context.Background()
+	t.Skip()
 
-	opts := &ArtisanalProviderURIOptions{
-		Pool:    "memory://",
-		Minimum: 5,
-		Clients: []string{
-			"brooklynintegers://",
-			"missionintegers://",
-		},
-	}
-
-	uri, err := NewArtisanalProviderURI(opts)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	str_uri := uri.String()
-
-	t.Log(str_uri)
-
-	pr, err := uid.NewProvider(ctx, str_uri)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	id, err := pr.UID()
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Log(id)
 }
